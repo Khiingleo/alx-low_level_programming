84b4b2fa50d0898301687592b8a7e515cbe2a39e
Shalom Michael <shalomzymike@gmail.com>
2023-03-16 15:28:56 +0000

    first c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 
-
 /**
- *main is the entry point
- *Return: return 0
+ * main - Entry point
+ * Return: Always 0 (success)
  */
-
 
 int main(void)
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
-}	
+}
