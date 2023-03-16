a4ff856e030e7c42801beff7497ef12cd6690857
Shalom Michael <shalomzymike@gmail.com>
2023-03-16 17:24:56 +0000

    print without using printf and puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 
+ */
+
+int main(void)
+{
+	system("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19'");
+	return (0);
+}
