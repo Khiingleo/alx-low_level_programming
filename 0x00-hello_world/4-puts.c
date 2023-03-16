b249064a9896eae4b2850922989ee3ae8451d6e6
shalom michael <shalomzymike@gmail.com>
2023-03-16 07:33:57 -0700

    first c code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+ *main is the entry point
+ *Return: return 0
+ */
+
+
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}	
