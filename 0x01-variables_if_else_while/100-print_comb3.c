5c2fd0dc5b538e43286819b8d12495b8b1f97772
Shalom Michael <shalomzymike@gmail.com>
2023-03-17 16:13:22 +0000

    possible combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int ones, tens;
+	
+	for (tens = '0'; tens <= '9'; tens++)
+	{
+		for(ones = (tens + 1); ones <= '9'; ones++)
+		{
+			putchar(tens);
+			putchar(ones);
+
+			if (tens != '8' || ones != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
