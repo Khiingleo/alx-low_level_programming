0d8d5a502afafb5cba330172fb6c844916c907cb
Shalom Michael <shalomzymike@gmail.com>
2023-03-17 13:59:57 +0000

    print alphabet using putchar and loops

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+
+int main(void)
+{
+	for (char alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
