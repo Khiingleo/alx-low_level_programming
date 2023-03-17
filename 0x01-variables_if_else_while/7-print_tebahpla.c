9bcac1bb7a3a2829a8469c60a49011774f8539d6
Shalom Michael <shalomzymike@gmail.com>
2023-03-17 15:03:33 +0000

    alphabets in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char reverseAlpha = 'z';
+
+	while (reverseAlpha > 'a')
+	{
+		putchar(reverseAlpha);
+		reverseAlpha--;
+	}
+	putchar('\n');
+	return (0);
+}
