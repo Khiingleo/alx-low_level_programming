6bb848e55af4ec4ca93117a0a41d04474036a035
Shalom Michael <shalomzymike@gmail.com>
2023-03-17 15:28:50 +0000

    print out single digits seperated by commas and spaces

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int nums = '0';
+
+	while (nums <= '9')
+	{
+		putchar(nums);
+		if(nums != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		nums++;
+	}
+	putchar('\n');
+	return (0);
+}
