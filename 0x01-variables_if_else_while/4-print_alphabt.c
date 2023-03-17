7561043406d80d2db21ef1d7714d9ca65f71f2f7
Shalom Michael <shalomzymike@gmail.com>
2023-03-17 14:23:10 +0000

    print out the alphabet except 1 and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		if (alpha == 'q' && alpha == 'e')
+		{
+			continue;
+		}
+		putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
+	return (0);
+}
