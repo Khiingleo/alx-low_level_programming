8b0e831e14662f7b57738a17482dd27fa5a33021
Shalom Michael <shalomzymike@gmail.com>
2023-03-21 17:02:46 +0000

    print times table of given number starting from 0

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ *print_times_table - prints time table of input starting from 0
+ *@n: integer argument
+ */
+
+void print_times_table(int n)
+{
+	int num, times, prod;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (num = 0; num <= n; num++)
+		{
+			_putchar('0');
+			for (times = 1; times <= n; times++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * times;
+
+				if (prod <= 99)
+				{
+					putchar(' ');
+				}
+				if (prod <= 9)
+				{
+					putchar(' ');
+				}
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
+				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -37,4 +37,7 @@ int add(int, int);
 void print_to_98(int n);
 /*print all natural numbers from n to 98*/
 
+void print_times_table(int n);
+/*print multiplication table*/
+
 #endif
