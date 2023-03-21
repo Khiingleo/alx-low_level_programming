30275507f130f8e39379fdee6cf02c1a2cbd1d07
Shalom Michael <shalomzymike@gmail.com>
2023-03-21 15:45:40 +0000

    print numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,17 +10,31 @@ void print_to_98(int n)
 {
 	while (n < 98)
 	{
-		printf("%d, ", n);
+		if (n == 98)
+		{
+			printf("%d", n);
+		}
+		else
+		{
+			printf("%d, ", n);
+		}
 		n++;
 	}
 	while (n > 98)
 	{
-		printf("%d, ", n);
+		if (n == 98)
+		{
+			printf("%d", n);
+		}
+		else
+		{
+			printf("%d, ", n);
+		}
 		n--;
 	}
 	if (n == 98)
 	{
-		printf("%d, ", n);
+		printf("%d", n);
 	}
 	printf("\n");
 }
