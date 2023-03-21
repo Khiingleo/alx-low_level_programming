9743b5c45db6944d3f03b33e26c8e880b098ab1d
Shalom Michael <shalomzymike@gmail.com>
2023-03-21 11:39:42 +0000

    print lowercase alphabets 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *print_alphabet_x10 - prints lower case alphabet 10 times 
+ *print_alphabet_x10 - prints lower case alphabet 10 times
  *Return: Return 0
  */
 
@@ -12,7 +12,7 @@ void print_alphabet_x10(void)
 
 	for (nums = 1; nums <= 10; nums++)
 	{
-		for(alpha = 'a'; alpha <= 'z'; alpha++)
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
 		{
 			_putchar(alpha);
 		}
