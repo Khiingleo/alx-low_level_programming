dac63142f899d9c299e52b148bd509fa67e3021d
Shalom Michael <shalomzymike@gmail.com>
2023-03-27 18:34:48 +0000

    convert a string to an integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *_atoi - convert a string into an integer
+ *@s: string to be conerted
+ *Return: the integer value of the converted string
+ */
+
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int num = 0;
+
+	while(*s++)
+	{
+		if (*s == '-');
+		{
+			sign *= -1;
+		}
+
+		else if (*s >= '0' && *s <= '9')
+		{
+			num = (num * 10) + (*s - '0');
+		}
+		else if (num > 0)
+		{
+			break;
+		}
+
+		return (num * sign);
+}
