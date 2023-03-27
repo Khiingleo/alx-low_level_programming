fc5d78ef82c1272b1d004cd7d58cb55dd5630738
Shalom Michael <shalomzymike@gmail.com>
2023-03-27 21:59:06 +0000

    update keygen file

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -13,15 +13,15 @@ int main(void) {
 	char password[PASSWORD_LENGTH];
 	int i;
 
-	srand(time(NULL));  // Seed the random number generator with the current time
+	srand(time(NULL));  /* Seed the random number generator with the current time */
 
 	for (i = 0; i < PASSWORD_LENGTH - 1; i++)
 	{
-		password[i] = rand() % 94 + 33; // Generate a random printable ASCII character
+		password[i] = rand() % 94 + 33; /* Generate a random printable ASCII character */
 	}
-	password[i] = '\0'; // Terminate the string with null character
+	password[i] = '\0'; /* Terminate the string with null character */
 
-	printf("%s\n", password); // Print the generated password
+	printf("%s\n", password); /* Print the generated password */
 
 	return 0;
 }
