e43ad820390810d19b2f2e25f64712c10b756eca
Shalom Michael <shalomzymike@gmail.com>
2023-03-27 17:15:59 +0000

    reverse an array

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *rev_string - reverses a string
+ *@s: string to be reversed
+ */
+
+void rev_string(char *s)
+{
+	int len = 0, index = 0;
+	char temp;
+
+	while (s[index++])
+	{
+		len++;
+	}
+
+	for (index = len - 1; index >= len / 2; index--)
+	{
+		temp = s[index];
+		s[index] = s[len - index - 1];
+		s[len - index - 1] = temp;
+	}
+}
