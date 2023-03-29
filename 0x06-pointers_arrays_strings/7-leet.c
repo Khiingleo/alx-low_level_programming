c07a4da53a687dfb3206d95b8d3c985abdbcfc41
Shalom Michael <shalomzymike@gmail.com>
2023-03-29 17:28:29 +0000

    encode a string int leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,7 +1,27 @@
 #include "main.h"
 
+/**
+ * leet - encodes into 1337 leet
+ * @str: string to be encoded
+ * Return: returns the string
+ */
 
-char *leet(char *)
+char *leet(char *str)
 {
+	int i;
+	int j;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
 
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (str[i] == a[j])
+			{
+				str[i] = b[j];
+			}
+		}
+	}
+	return (str);
 }
