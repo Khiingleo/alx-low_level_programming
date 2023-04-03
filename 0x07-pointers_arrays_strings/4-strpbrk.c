7a51edbd37f95201cf33072c66f5ba34bccbd8d1
Shalom Michael <shalomzymike@gmail.com>
2023-04-03 14:26:52 +0000

    made changes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,17 +9,19 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	int i, j;
+	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s)
 	{
-		for (j = 0; accept[j] != '\0'; j++)
+		for (i = 0; accept[i] != '\0'; i++)
 		{
-			if (s[i] == accept[j])
+			if (*s == accept[i])
 			{
-				return (s + i);
+				return (s);
 			}
 		}
+
+		s++;
 	}
 	return ('\0');
 
