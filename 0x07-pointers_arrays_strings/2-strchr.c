43150380080e10f1db7c7bcb234c07c2c7306d05
Shalom Michael <shalomzymike@gmail.com>
2023-04-03 12:33:56 +0000

    create strchr function

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ * _strchr - finds the first occurrence of c in the string
+ * @s: string to search for the character c
+ * @c: character to search for
+ * Return: pointer to character
  */
 
 char *_strchr(char *s, char c)
@@ -13,8 +14,9 @@ char *_strchr(char *s, char c)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
-			return (s + i);/*returns i + the first element of the string*/
+			return (s + i);
+		/*the above code adds i to the address of the first element of s*/
+		/*thereby returning a new pointer to the resulting memory location*/
 	}
 	return ('\0');
-	
 }
