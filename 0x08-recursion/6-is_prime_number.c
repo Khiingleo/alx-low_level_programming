aa2ae7e698ce0ceda1334d008240d90df96e9b9f
Shalom Michael <shalomzymike@gmail.com>
2023-04-05 14:37:55 +0000

    betty documentation

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * prime - recursively divide by higher divisor, skip even numbers
+ * @n: number to check
+ * @div: divisor
+ * Return: 1 if prime, 0 if not, or recursive function call
+ */
 
 int prime(int n, int div)
 {
@@ -11,7 +17,11 @@ int prime(int n, int div)
 	return (prime(n, div + 1));
 }
 
-
+/**
+ * is_prime_number - check if prime
+ * @n: number to check
+ * Return: 1 if prime and 0 if not
+ */
 
 
 int is_prime_number(int n)
