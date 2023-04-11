50b998ea65696ed02d7bb117ecd6c3c5e1644aaa
Shalom Michael <shalomzymike@gmail.com>
2023-04-11 16:56:09 +0000

    made changes

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,6 +20,9 @@ char *create_array(unsigned int size, char c)
 
 	ar = malloc(size * sizeof(char));
 
+	if (ar == NULL)
+		return (NULL);
+
 	for (i = 0; i < size; i++)
 		ar[i] = c;
 	return (ar);
