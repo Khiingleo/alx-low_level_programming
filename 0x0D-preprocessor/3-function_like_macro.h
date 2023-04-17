8d2a4a55445d3cfb8492d0a79f42feaecd647e44
Shalom Michael <shalomzymike@gmail.com>
2023-04-17 18:19:27 +0000

    fix code

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,14 @@
 #ifndef FUNCTION_LIKE_MACRO
 #define FUNCTION_LIKE_MACRO
 
-#define ABS(x) (((x) < 0) ? (-x) : (x))
+/*
+ * File: 3-function_like_macro.h
+ * Auth: Shalom Ogoziem
+ * Desc: Header file that defines a function-like macro ABS(x)
+ *       that computes the absolute value of a number x.
+ */
+
+#define ABS(x) ((x) < 0 ? (-x) : (x))
 
 #endif
 
