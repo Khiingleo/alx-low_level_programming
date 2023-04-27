b614e1cb4e6be2aaafe2584008dd6402ebfbd678
Shalom Michael <shalomzymike@gmail.com>
2023-04-27 21:02:57 +0000

    print Hello, Holberton using assembly

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,20 @@
+section .data
+	hello db 'Hello, Holberton', 0
+	format db '%s\n', 0
+
+section .text
+	global main
+	extern printf
+
+main:
+	push rbp
+	mov rbp, rsp
+
+	push qword msg
+	push qword format
+	call printf
+	add rsp 16
+
+	mov rsp, rbp
+	pop rbp
+	ret
