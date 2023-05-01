136604893a6146732230314b727dd2be841a6731
Shalom Michael <shalomzymike@gmail.com>
2023-05-01 11:30:34 +0000

    fucntion thar returns the num of elements in a list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,21 +1,22 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a listint_t list
+ * listint_len - returns the number of elements in a
+ * listint_t list
  * @h: the listint_t list
- * Return: the number of nodes in the list
+ * Return: number of elements
  */
 
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	const listint_t *temp = h;
 	size_t count = 0;
 
 	while (temp != NULL)
 	{
-		printf("%d\n", temp->n);
 		count++;
 		temp = temp->next;
 	}
+
 	return (count);
 }
