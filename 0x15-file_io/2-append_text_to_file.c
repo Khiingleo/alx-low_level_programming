40767a1e13bddef277472d6200df3f808bbfc30e
Shalom Michael <shalomzymike@gmail.com>
2023-05-08 21:36:13 +0000

    add text at end of file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,7 +16,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (textcontent != NULL)
+	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
 			len++;
