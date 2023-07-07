a23dbd00557160eaa45c1903426fb84e605170bd
Shalom Ogoziem <shalomzymike@gmail.com>
2023-07-07 10:09:58 +0000

    fix task

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,9 +12,14 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new, *current;
+	char *val_copy;
 	unsigned long int index;
 
 	if (key == NULL || *key == '\0' || value == NULL || ht == NULL)
+		return (0);
+
+	val_copy = strdup(value);
+	if (val_copy == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
@@ -25,19 +30,27 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (strcmp(current->key, key) == 0)
 		{
 			free(current->value);
-			current->value = strdup(value);
+			current->value = val_copy;
 			return (1);
 		}
 		current = current->next;
 	}
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
+	{
+		free(val_copy);
 		return (0);
+	}
 
 	new->key = strdup(key);
-	new->value = strdup(value);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0)
+	}
+	new->value = val_copy;
 	new->next = ht->array[index];
 	ht->array[index] = new;
 
-	return (0);
+	return (1);
 }
